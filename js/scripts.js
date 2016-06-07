561bee0af60bd2b64f2d08d00a1a881b1e04686f
Benjamin Heflin and Rony Nasr <benjamin+rony@>
2016-06-07 13:54:15 -0700

    added complete project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-/*This file is for your custom js.  All yours*/
+// back end
+var factorial= function(number){
+  console.log(number);
+  var result= 1;
+  for(var i = 1; i <= number; i+=1) {
+    result *= i;
+  }
+  return result;
+}
+
+
+// front end
+$(function() {
+  $("form#input").submit(function(event) {
+
+    //input
+    var num = parseInt($("#number").val());
+    console.log(num);
+    //output
+    $("#output").text(factorial(num));
+    event.preventDefault();
+  });
+});
